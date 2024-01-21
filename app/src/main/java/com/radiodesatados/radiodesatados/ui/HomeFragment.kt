6035e6adcb8ecbce37e3f1b4639593f4f93f9e81
Alex Muñoz <lomejor77@gmail.com>
2024-01-21 11:08:10 -0300

    cambios realizados: se agregan funciones de muteo y desmuteo, animacion del logo en el fragment splash

--- app/src/main/java/com/radiodesatados/radiodesatados/ui/HomeFragment.kt
+++ app/src/main/java/com/radiodesatados/radiodesatados/ui/HomeFragment.kt
@@ -3,6 +3,7 @@ package com.radiodesatados.radiodesatados.ui
 import android.content.Context
 import android.media.AudioAttributes
 import android.media.AudioManager
+import android.media.MediaMetadataRetriever
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -11,15 +12,16 @@ import android.view.ViewGroup
 import android.widget.SeekBar
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.radiodesatados.radiodesatados.R
+import com.radiodesatados.radiodesatados.R.drawable
 import com.radiodesatados.radiodesatados.databinding.FragmentHomeBinding
 
 
 
 class HomeFragment: Fragment() {
     private lateinit var binding: FragmentHomeBinding
-    private lateinit var MPlayer: MediaPlayer
+    private lateinit var mPlayer: MediaPlayer
     private lateinit var  audioManager: AudioManager
+    private var isMuted = false
 
 
     override fun onCreateView(
@@ -28,37 +30,63 @@ class HomeFragment: Fragment() {
     ): View {
        binding = FragmentHomeBinding.inflate(layoutInflater,container,false)
 
-        Radio()
+
+        radio()
         binding.volumeId.max = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC)
         binding.volumeId.progress = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
+
+        // Botones para mutear/desmutear
+        val muteButton = binding.mute
+        val unmuteButton = binding.unMute
+        muteButton.setOnClickListener {
+            toggleMute()
+        }
+
+        unmuteButton.setOnClickListener {
+            toggleMute()
+        }
         //whatsapp()
 
         return binding.root
-
-
-
     }
 
-    private fun Radio() {
+
+    private fun radio() {
         this.audioManager = requireContext().getSystemService(Context.AUDIO_SERVICE) as AudioManager
         val url = "https://stream-150.zeno.fm/f0w58gzhrc9uv"
-        MPlayer = MediaPlayer()
-        MPlayer.setDataSource(url)
-        MPlayer.setAudioAttributes(AudioAttributes.Builder()
+        mPlayer = MediaPlayer()
+        mPlayer.setDataSource(url)
+        mPlayer.setOnInfoListener { _, what, _ ->
+            if (what == MediaPlayer.MEDIA_INFO_METADATA_UPDATE) {
+                val metadataRetriever = MediaMetadataRetriever()
+                metadataRetriever.setDataSource(url)
+
+                val title = metadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE)
+                val artist = metadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST)
+
+                val metadataText = "Title: $title\nArtist: $artist"
+
+                // Actualizar el TextView con la información de la metadata
+                binding.metaText.text = metadataText
+            }
+            true
+        }
+
+        mPlayer.setAudioAttributes(AudioAttributes.Builder()
                 .setUsage(AudioAttributes.USAGE_MEDIA)
                 .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
                 .build()
         )
-        MPlayer.setOnErrorListener { mediaPlayer, what, extra -> // logging stuff
+        mPlayer.setOnErrorListener { _, what, _ -> // logging stuff
             //Log.e("asdasd","asdsad")
             Toast.makeText(requireActivity(), "Revisa tu conexion a internet", Toast.LENGTH_SHORT)
                 .show()
             false
         }
         //MPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC)
-        MPlayer.prepareAsync()
+        mPlayer.prepareAsync()
 
-        MPlayer.setOnPreparedListener {
+        mPlayer.setOnPreparedListener {
             play()
         }
     }
@@ -67,15 +95,15 @@ class HomeFragment: Fragment() {
 
         binding.btnPlay.setOnClickListener {
 
-            if(MPlayer.isPlaying)
+            if(mPlayer.isPlaying)
             {
-                MPlayer.pause()
+                mPlayer.pause()
                 Toast.makeText(requireActivity(), "En Pausa", Toast.LENGTH_SHORT).show()
-                binding.btnPlay.setBackgroundResource(R.drawable.baseline_play_circle_filled_24)
+                binding.btnPlay.setBackgroundResource(drawable.baseline_play_circle_filled_24)
             }else{
-                MPlayer.start()
-                Toast.makeText(requireActivity(), "Escuchando...", Toast.LENGTH_SHORT).show()
-                binding.btnPlay.setBackgroundResource(R.drawable.baseline_pause_circle_outline_24)
+                mPlayer.start()
+                Toast.makeText(requireActivity(), "Estás escuchando Radio Desatados...", Toast.LENGTH_SHORT).show()
+                binding.btnPlay.setBackgroundResource(drawable.baseline_pause_circle_outline_24)
             }
             binding.volumeId.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
                 override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
@@ -87,6 +115,29 @@ class HomeFragment: Fragment() {
 
                 override fun onStopTrackingTouch(seekBar: SeekBar?) {}
             })
+        }
+    }
+
+    private fun toggleMute() {
+        if (mPlayer != null) {
+            if (isMuted) {
+                // Habilita el sonido (establece el volumen a su valor original)
+                mPlayer!!.setVolume(1f, 1f)
+                Toast.makeText(
+                    requireActivity(),
+                    "Radio con volumen activado.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                // Mutea el sonido (establece el volumen a 0)
+                mPlayer!!.setVolume(0f, 0f)
+                Toast.makeText(
+                    requireActivity(),
+                    "Radio silenciada.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            isMuted = !isMuted
         }
     }
 
--- app/src/main/java/com/radiodesatados/radiodesatados/ui/SplahFragment.kt
+++ app/src/main/java/com/radiodesatados/radiodesatados/ui/SplahFragment.kt
@@ -1,12 +1,14 @@
 package com.radiodesatados.radiodesatados.ui
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.LinearInterpolator
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.radiodesatados.radiodesatados.R
@@ -26,9 +28,35 @@ class SplahFragment : Fragment() {
         }, 3500)
         binding = FragmentSplahBinding.inflate(layoutInflater, container, false)
        // SplashView()
-
+       textAnimate()
 
         return binding.root
+    }
+
+    private fun textAnimate() {
+
+        // Asigna tu TextView o cualquier vista que desees animar
+        val titleTextView: View = binding.imageView
+        // Crea una animación que desplaza la vista hacia arriba y hacia abajo
+        val animator = ObjectAnimator.ofFloat(
+            titleTextView,
+            "translationY",
+            50f,
+            -50f
+        )
+
+        // Configura la duración de la animación (en milisegundos)
+        animator.duration = 5000
+
+        // Configura el interpolador para un movimiento uniforme
+        animator.interpolator = LinearInterpolator()
+
+        // Repite la animación indefinidamente
+        animator.repeatCount = ObjectAnimator.INFINITE
+        animator.repeatMode = ObjectAnimator.REVERSE
+
+        // Inicia la animación
+        animator.start()
     }
 
     /**private fun SplashView() {
